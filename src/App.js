38c47e9409326d61a9eba9bfd09ddf3d18c15d0e
LIHA11 <LIHA11@OOCL.com>
2025-09-12 14:50:32 +0800

    initState commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import {createContext, useContext, useReducer} from "react";
+
+export const initState = [
+    {id: 1, text: "the first todo", done: false},
+    {id: 2, text: "the second todo", done: false},
+];
+export const TodoContext = createContext()
+
+function TodoItem(props) {
+    return <div className={"todo-item"}>
+        {props.todo.text}
+    </div>
+}
+
+function TodoGroup() {
+    const {state, dispatch} = useContext(TodoContext)
+    return <div>
+        {
+            state.map((item, index) => {
+                return <TodoItem todo={item} key={index} index={index}/>
+            })
+        }
+    </div>
+}
+
+export function todoReducer(state, action) {
+    return state
+}
+
+function App() {
+    const [state, dispatch] = useReducer(todoReducer, initState);
+    return (
+        <div>
+            <TodoContext.Provider value={{state, dispatch}}>
+                <TodoGroup/>
+            </TodoContext.Provider>
+        </div>
+    );
+}
+
+export default App;
