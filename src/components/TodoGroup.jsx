be5a20e6baa77d71ed07f06dce5145d9f6b98152
LIHA11 <LIHA11@OOCL.com>
2025-09-12 20:58:59 +0800

    feat: Implement delete function

--- src/components/TodoGroup.jsx
+++ src/components/TodoGroup.jsx
@@ -3,12 +3,25 @@ import {TodoItem} from "./TodoItem";
 
 import {TodoContext} from "../contexts/TodoContext";
 
+
 export function TodoGroup() {
     const {state, dispatch} = useContext(TodoContext)
+
+function deleteItem(id, done) {
+    if (!done) {
+        alert("You can only delete a task that is marked as done.");
+        return;
+    }
+    dispatch({ type: "DELETE_TODO", payload: { id } });
+}
+
     return <div>
         {
             state.map((item, index) => {
-                return <TodoItem todo={item} key={index} index={index}/>
+                return <div className={"todo-group"} key={index}>
+                    <TodoItem todo={item} key={index} index={index}/>
+                    <button onClick={() => deleteItem(item.id, item.done)}>X</button>
+                </div>
             })
         }
     </div>
