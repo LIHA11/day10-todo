255346ca8a2b636173ac0205060842c4cd174cec
LIHA11 <LIHA11@OOCL.com>
2025-09-12 21:13:25 +0800

    fix bug: remove repeat to-do item

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,14 @@
+import {TodoGroup} from './TodoGroup';
 import {useContext} from 'react';
 import {TodoContext} from '../contexts/TodoContext';
 import {TodoItem} from './TodoItem';
 
-export function TodoList({children}) {
-    const {state} = useContext(TodoContext);
+export function TodoList() {
     return (
         <div className="todo-list-container">
             <h1 className="todo-list-title">Todo List</h1>
             <div className="todo-list-subtitle">Add the things you need to do today...</div>
-            <ul>
-                {state.map(todo => (
-                    <TodoItem key={todo.id} todo={todo} />
-                ))}
-            </ul>
-            {children}
+            <TodoGroup />
         </div>
     );
 }
