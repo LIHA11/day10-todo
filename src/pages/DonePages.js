24b99dc8ee93257abdae0666fce7f77c27b50819
LIHA11 <LIHA11@OOCL.com>
2025-09-15 20:06:11 +0800

    refactor: modify css in done page

--- src/pages/DonePages.js
+++ src/pages/DonePages.js
@@ -5,25 +5,8 @@ import {ToDoItem} from "../components/ToDoItem";
 import { Checkbox, Divider, Button, Flex, Progress, Space } from 'antd';
 import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
 
-const CheckboxGroup = Checkbox.Group;
-const weekOptions = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-const defaultCheckedList = ['Monday', 'Friday'];
-
 export function DonePages() {
     const {state} = useContext(TodoContext);
-    const [checkedList, setCheckedList] = useState(defaultCheckedList);
-    const checkAll = weekOptions.length === checkedList.length;
-    const indeterminate = checkedList.length > 0 && checkedList.length < weekOptions.length;
-
-    const onChange = (list) => {
-        setCheckedList(list);
-    };
-
-    const onCheckAllChange = (e) => {
-        setCheckedList(e.target.checked ? weekOptions : []);
-    };
-
-    // 新增进度条和按钮功能
     const [percent, setPercent] = useState(0);
     const increase = () => {
         setPercent(prevPercent => {
@@ -45,22 +28,18 @@ export function DonePages() {
     };
 
     return <div>
-        {/* 可调节进度条和按钮 */}
         <Flex vertical gap="small" style={{ margin: '24px auto', maxWidth: 220 }}>
             <Flex vertical gap="small">
                 <Progress percent={percent} type="line" />
                 <Progress percent={percent} type="circle" />
             </Flex>
-            <Space.Compact>
-                <Button onClick={decline} icon={<MinusOutlined />} />
-                <Button onClick={increase} icon={<PlusOutlined />} />
-            </Space.Compact>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Space.Compact>
+                    <Button onClick={decline} icon={<MinusOutlined />} />
+                    <Button onClick={increase} icon={<PlusOutlined />} />
+                </Space.Compact>
+            </div>
         </Flex>
-        <Checkbox indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll}>
-            Check all
-        </Checkbox>
-        <Divider />
-        <CheckboxGroup options={weekOptions} value={checkedList} onChange={onChange} />
         <Divider />
         {
             state.filter(item => item.done).map((item, index) => (
