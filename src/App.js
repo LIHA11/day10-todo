9d8a99d85c01f2ce36a5508418238201f65ffeb0
LIHA11 <LIHA11@OOCL.com>
2025-09-15 10:43:06 +0800

    feat: NavLink

--- src/App.js
+++ src/App.js
@@ -3,12 +3,33 @@ import {useReducer} from "react";
 import {todoReducer} from "./reducers/TodoReducer";
 import {TodoContext} from "./contexts/TodoContext";
 import {TodoList} from "./components/TodoList";
-import {createBrowserRouter, RouterProvider} from "react-router";
+import {createBrowserRouter, NavLink, Outlet, RouterProvider} from "react-router";
+
+function DefaultLayout() {
+    return <div>
+        <header>
+            <nav>
+                <ul>
+                    <li><NavLink to = {"/"}>Home</NavLink></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <Outlet />
+        </main>
+    </div>
+}
 
 const routes = createBrowserRouter([
     {
         path:"/",
-        element:<TodoList/>
+        element:<DefaultLayout/>,
+        children:[
+            {
+                path:"/",
+                element:<TodoList/>
+            }
+        ]
     }
 ]);
 
