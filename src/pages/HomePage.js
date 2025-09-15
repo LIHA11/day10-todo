bb538668da7c0dd93a55b08a980714cb3ff50e5c
LIHA11 <LIHA11@OOCL.com>
2025-09-15 20:00:31 +0800

    feat:add circular progress bar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,16 @@
+import React from 'react';
 import {TodoList} from "../components/TodoList";
+import { Flex, Progress } from 'antd';
 
 export function HomePage() {
-    return <TodoList/>;
+    return (
+        <div>
+            <Flex wrap gap="small" style={{ margin: '24px auto', maxWidth: 300 }}>
+                <Progress type="circle" percent={30} size={80} />
+                <Progress type="circle" percent={70} size={80} status="exception" />
+                <Progress type="circle" percent={100} size={80} />
+            </Flex>
+            <TodoList/>
+        </div>
+    );
 }
