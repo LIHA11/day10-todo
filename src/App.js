22e3e2e291e27f870bf93a16381f51e726b04114
LIHA11 <LIHA11@OOCL.com>
2025-09-15 10:53:34 +0800

    refactor: extract to pages directory

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import {useReducer} from "react";
 import {todoReducer} from "./reducers/TodoReducer";
 import {TodoContext} from "./contexts/TodoContext";
-import {TodoList} from "./components/TodoList";
-import {createBrowserRouter, NavLink, Outlet, RouterProvider, useRouteError} from "react-router";
+import {createBrowserRouter, NavLink, Outlet, RouterProvider} from "react-router";
+import {ErrorPage} from "./pages/ErrorPage";
+import {HomePage} from "./pages/HomePage";
 
 function DefaultLayout() {
     return <div>
@@ -20,23 +21,6 @@ function DefaultLayout() {
     </div>
 }
 
-function ErrorPage() {
-    const error = useRouteError();
-    return (
-        <div className="error-page">
-            {error.status === 404 ? (
-                <div className="not-found">
-                    <h1>404</h1>
-                    <p>页面未找到</p>
-                    <a href="/" className="back-home">返回首页</a>
-                </div>
-            ) : (
-                <div className="error-info">{JSON.stringify(error)}</div>
-            )}
-        </div>
-    );
-}
-
 
 const routes = createBrowserRouter([
     {
@@ -46,7 +30,7 @@ const routes = createBrowserRouter([
         children:[
             {
                 path:"/",
-                element:<TodoList/>
+                element:<HomePage/>
             }
         ]
     }
