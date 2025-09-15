4f9b9a1d1134c2bc6798f5040ca8fc8061d03f95
LIHA11 <LIHA11@OOCL.com>
2025-09-15 15:27:36 +0800

    refactor(WIP): extract deleteItem

--- src/components/TodoGenerator.jsx
+++ src/components/TodoGenerator.jsx
@@ -3,20 +3,28 @@ import {TodoContext} from '../contexts/TodoContext';
 import './TodoGenerator.css';
 import {api} from "../api/mockApi";
 
+
+const createTodo = (text)  => {
+    return api.post("/todos",
+        {text: text.trim(), done: false})
+        .then((res => res.data));
+}
+
+
+
+
 export function TodoGenerator() {
     const {dispatch, state} = useContext(TodoContext);
     const [text, setText] = useState('');
 
     const handleAdd = (e) => {
         if (text.trim() === '') return;
-        api.post("/todos",
-            {text: text.trim(), done: false})
-            .then((res => res.data))
+        createTodo(text)
             .then(todo =>
                 dispatch({type: 'ADD_TODO', payload: todo})
             )
             .catch(err => console.error("Failed to add todo:", err));
-
+        //
         setText('');
     };
 
