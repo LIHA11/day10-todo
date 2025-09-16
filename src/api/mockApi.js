8242269e10e77c5f59bbe341f5cc90fcb11a3d7e
LIHA11 <LIHA11@OOCL.com>
2025-09-16 10:12:02 +0800

    feat:add 404 alert

--- src/api/mockApi.js
+++ src/api/mockApi.js
@@ -1,7 +1,25 @@
 import axios from "axios";
+import {message} from "antd";
 
-export const api = axios.create({
-    baseURL: 'https://68c7ac8f5d8d9f51473287a8.mockapi.io/',
+const api = axios.create({
+    baseURL: 'https://68c7ac8f5d8d9f51473287a8.mockapi.io/123456',
     headers: {'Content-Type': 'application/json'},
     timeout: 10_000
 })
+
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        const {status} = error.response;
+        if (status === 404) {
+            alert(error.message);
+            // message.error(error.message).then(r => {});
+
+        }
+        return Promise.reject(error);
+    }
+);
+
+export {api}
