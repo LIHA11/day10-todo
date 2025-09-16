d69f6b6c3652272fc3aa93a751b769386d9ff2ef
LIHA11 <LIHA11@OOCL.com>
2025-09-16 20:45:14 +0800

    feat:change baseURL

--- src/api/mockApi.js
+++ src/api/mockApi.js
@@ -3,7 +3,8 @@ import '@ant-design/v5-patch-for-react-19';
 import {message} from "antd";
 
 const api = axios.create({
-    baseURL: 'https://68c7ac8f5d8d9f51473287a8.mockapi.io/123456',
+    baseURL:'http://localhost:8081/',
+    // baseURL: 'https://68c7ac8f5d8d9f51473287a8.mockapi.io/123456',
     headers: {'Content-Type': 'application/json'},
     timeout: 10_000
 })
